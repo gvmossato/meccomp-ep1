c96fad395aad6b53fdcbfbc006925e94f593ad88
Higor Fellipe de Almeida Silva <higor_silva@usp.br>
2022-05-31 15:13:29 -0300

    Using the Mash_grid as a function input

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-def test_h(h,contours_pos,grid_limits):
-    mash_grid = np.linspace(grid_limits[0],grid_limits[1]+h,h)
+def test_h(h,contours_pos,mash_grid)
 
     return np.all([e in mash_grid for e in np.array(contours_pos)])
